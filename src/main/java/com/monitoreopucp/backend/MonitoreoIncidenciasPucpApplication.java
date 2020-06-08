5a3d5819d01ba78ccbf8eedd4ac50c2dbc4ff21f
Eddy Rodriguez <e.rodriguezd@pucp.edu.pe>
2020-06-07 19:02:45 -0500

    Initial commit

---
+++ src/main/java/com/monitoreopucp/backend/MonitoreoIncidenciasPucpApplication.java
@@ -0,0 +1,13 @@
+package com.monitoreopucp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoreoIncidenciasPucpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoreoIncidenciasPucpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monitoreopucp/backend/MonitoreoIncidenciasPucpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitoreopucp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoreoIncidenciasPucpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
